5b60d43e39fad33a67783ab6ff37a2d7d3bea6c0
Mipjeiger <dianhinhin123@gmail.com>
2026-01-04 19:12:01 +0800

    add sql project

---
+++ sql/data.sql
@@ -0,0 +1,4 @@
+SELECT * FROM raw.pharmacy_sales; -- Ingest data from Pharmacy sales big project
+SELECT * FROM features.sales_llm_context; -- Ingest data from Pharmacy sales big project
+SELECT * FROM features.sales_feature; -- Ingest data from Pharmacy sales big project
+SELECT * FROM features.pharmacy_sales_enhanced; -- Ingest data from Pharmacy sales big project
